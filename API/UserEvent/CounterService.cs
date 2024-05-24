f3c8a24c15b1ba1ea5e602d2b4da0019c6b77a22
akchandra-jobs <167281374+akchandra-jobs@users.noreply.github.com>
2024-05-24 11:27:34 +0530

    Add files via upload

---
+++ API/UserEvent/CounterService.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace ProcessFlow.Services
+{
+    public interface ICounterService
+    {
+        string GetCounter();
+    }
+    public class CounterService : ICounterService
+    {
+        private static readonly Random random = new Random();
+
+        public string GetCounter()
+        {
+            int length = 10; // Length of the random string
+            return GenerateRandomString(length);
+        }
+
+        private string GenerateRandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            StringBuilder result = new StringBuilder(length);
+
+            for (int i = 0; i < length; i++)
+            {
+                result.Append(chars[random.Next(chars.Length)]);
+            }
+
+            return result.ToString();
+        }
+    }
+}
